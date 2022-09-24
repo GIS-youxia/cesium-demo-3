83ad9b782aea1985883d6eb0f6ae820b78709205
lianming <1097364388@qq.com>
2022-09-24 17:32:24 +0800

    add

---
+++ case/mjq/app.js
@@ -0,0 +1,89 @@
+import * as Cesium from 'cesium';
+import './src/Cesium_EX'
+// @ts-ignore
+window.Cesium = Cesium;
+// @ts-ignore
+console.log(Cesium.VERSION);
+
+
+/*--------------------------------------- tool ------------------------------------------ */
+import { addAxisGlobe } from './tool/axis'
+import { updateResolutionScale, getClickPointAdd, addPoint } from './tool/tool'
+import { setCamera, setComeraPosition } from './tool/camera'
+import { addGaode } from './tool/provider';
+
+/*--------------------------------------- case ------------------------------------------ */
+import { coneRotation } from './show'
+import { MjqGrid } from './case/mjq/mjq_grid'
+
+// @ts-ignore
+window.CESIUM_BASE_URL = "./node_modules/cesium/Build/CesiumUnminified";
+Cesium.Ion.defaultAccessToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3NTM5NzhjYy0yYzRjLTQwYWUtODA1NC1lMTRiMzIyOWFkZmYiLCJpZCI6NTU4NzksImlhdCI6MTYyMTM3Nzc5OX0.vqIWYzqg7zdA9ubxUBVY5bUCSQ3NnsSsaE8VI_A8ThU`;
+const viewOptions = {
+  //-------------------------- UI控件 --------------------
+  //动画
+  animation: false,
+  // 图层选择
+  baseLayerPicker: false,
+  // 全屏
+  fullscreenButton: false,
+  infoBox: false,
+  // 2d/3d切换
+  sceneModePicker: false,
+  selectionIndicator: false,
+  timeline: false,
+  navigationHelpButton: false,
+  shouldAnimate: false,
+  imageryProvider: undefined,
+}
+
+var viewer = new Cesium.Viewer("cesiumContainer", viewOptions);
+// @ts-ignore
+window.viewer = viewer;
+
+viewer.shadows = false;
+
+// addGaode(viewer)
+// setCamera(viewer, "beiJing")
+
+// 解决模糊
+updateResolutionScale(viewer)
+
+// 显示帧率
+// @ts-ignore
+viewer.scene.debugShowFramesPerSecond = true;
+
+// 开启拾取
+// getClickPointAdd(viewer, pos => {
+//   // addAxis(viewer, pos);
+// });
+
+// @ts-ignore
+// viewer.scene.globe.show = false;
+// @ts-ignore
+viewer.scene.globe.enableLighting = false;
+// @ts-ignore
+viewer.shadows = false;
+// @ts-ignore
+viewer.scene.sun.show = false;
+// @ts-ignore
+viewer.scene.moon.show = false;
+// @ts-ignore
+viewer.scene.skyAtmosphere.show = false;
+// @ts-ignore
+viewer.scene.fog.enable = false;
+viewer.scene.globe.enableLighting = false;
+
+// 添加地球坐标轴
+// addAxisGlobe(viewer)
+// coneRotation(viewer);
+
+// setComeraPosition(viewer, {
+//   longitude: 116.4715397068917,
+//   latitude: 39.9945712104195,
+//   height: 100000,
+// })
+
+
+// fnLoadKML(viewer, "./res/daolubiaozhu.kml")
+window.mjq = new MjqGrid(viewer)
