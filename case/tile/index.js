2ed73696c2e0b82dc95666cbaec705bccfd434f9
lianming <1097364388@qq.com>
2022-09-26 22:46:35 +0800

    add

--- case/tile/index.js
+++ case/tile/index.js
@@ -2,19 +2,30 @@ import * as Cesium from 'cesium';
 
 export class Tile {
   constructor(viewer) {
+    this.init(viewer);
+  }
+
+  async init(viewer) {
+    await this.addMap(viewer);
+    this.addTile(viewer);
+   }
+
+  addTile() {
     const tileset = viewer.scene.primitives.add(
       new Cesium.Cesium3DTileset({
         url: "./res/tileset.json",
+        // url: "https://lab.earthsdk.com/model/702aa950d03c11e99f7ddd77cbe22fea/tileset.json"
+        // url: "http://211.149.185.229:8081/data/country_build/chengdu_ajust_clip/tileset.json"
       })
     );
 
 
     tileset.readyPromise.then(function (tileset) {
-      viewer.zoomTo(tileset);
+      viewer.flyTo(tileset);
       tileset.style = new Cesium.Cesium3DTileStyle({
         color: {
           conditions: [
-            ['true', 'rgba(0, 127.5, 255 ,1)']
+            ['true', 'rgba(0, 127, 255 ,1)']
           ]
         }
       });
@@ -23,15 +34,25 @@ export class Tile {
         lightingModel: Cesium.LightingModel.UNLIT,
         fragmentShaderText: `
                     void fragmentMain(FragmentInput fsInput, inout czm_modelMaterial material) {
-                        float _baseHeight = 0.0; // 物体的基础高度，需要修改成一个合适的建筑基础高度
-                        float _heightRange = 60.0; // 高亮的范围(_baseHeight ~ _baseHeight + _      heightRange) 默认是 0-60米
-                        float _glowRange = 300.0; // 光环的移动范围(高度)
-                        float vtxf_height = fsInput.attributes.positionMC.z-_baseHeight;
-                        float vtxf_a11 = fract(czm_frameNumber / 120.0) * 3.14159265 * 2.0;
+                        // 物体的基础高度，需要修改成一个合适的建筑基础高度
+                        float _baseHeight = 0.0;
+
+                        // 高亮的范围(_baseHeight ~ _baseHeight + _heightRange) 默认是 0-60米
+                        float _heightRange = 60.0;
+
+                        // float _glowRange = 300.0; // 光环的移动范围(高度)
+                        float _glowRange = 0.0; // 光环的移动范围(高度)
+
+                        float vtxf_height = fsInput.attributes.positionMC.z - _baseHeight;
+
+                        float vtxf_a11 = fract(czm_frameNumber / 120.0) * 3.14159265 * 1.;
+
                         float vtxf_a12 = vtxf_height / _heightRange + sin(vtxf_a11) * 0.1;
-                        material.diffuse*= vec3(vtxf_a12, vtxf_a12, vtxf_a12);
+
+                        material.diffuse *= vec3(vtxf_a12);
                         float vtxf_a13 = fract(czm_frameNumber / 360.0);
                         float vtxf_h = clamp(vtxf_height / _glowRange, 0.0, 1.0);
+
                         vtxf_a13 = abs(vtxf_a13 - 0.5) * 2.0;
                         float vtxf_diff = step(0.005, abs(vtxf_h - vtxf_a13));
                         material.diffuse += material.diffuse * (1.0 - vtxf_diff);
@@ -40,8 +61,26 @@ export class Tile {
       });
       tileset.customShader = customShader;
     });
-    window.tileset = tileset;
-
+  }
+  addMap(viewer) {
+    return new Promise((resolve, reject) => {
+      const imageryProvider = new Cesium.ArcGisMapServerImageryProvider({
+        // url: 'http://map.geoq.cn/arcgis/rest/services/ChinaOnlineStreetPurplishBlue/MapServer'
+        // url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer'
+        url: "http://map.geoq.cn/arcgis/rest/services/ChinaOnlineStreetPurplishBlue/MapServer"
+      });
+      viewer.imageryLayers.addImageryProvider(imageryProvider);
+      // var options = {
+      //   style: 'vec', // style: img vec normal dark
+      //   crs: 'WGS84' // 使用84坐标系，默认为：BD09
+      // }
+      // var imageryProvider = new Cesium.BaiduImageryProvider(options)
+      // viewer.imageryLayers.addImageryProvider(
+      //   imageryProvider
+      // )
+      // resolve();
+      imageryProvider.readyPromise.then(resolve)
+    })
 
   }
 }
