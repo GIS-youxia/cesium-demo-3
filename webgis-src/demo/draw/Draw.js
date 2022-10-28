87e66bebe35a91b4684f75159a33573206446b10
lianming <15127004511@qq.com>
2022-10-28 16:59:39 +0800

    add 添加绘制交互

--- webgis-src/demo/draw/Draw.js
+++ webgis-src/demo/draw/Draw.js
@@ -4,192 +4,151 @@ import * as Cesium from 'cesium';
 import { addGaode, addDebug } from '../../../tool/provider';
 import { addCSS } from '../../ui/helper';
 import { CesiumDrawingTool } from './cesium-drawing-tool'
-
+import { PolylineTool } from './PolylineTool'
+import { MarkerTool } from './MarkerTool'
+import {PolygonTool} from './PolygonTool'
 addCSS("./webgis-src/demo/draw/cesium-drawing-tool.css")
+
 const DrawState = {
   "NONE": "none",
   "MARKER": "marker",
   "POLYLINE": "polyline",
 }
+
 export class DrawDemo {
   constructor(viewer) {
-    this.viewer = viewer
+    this._viewer = viewer
     window.aaa = this;
     // addGaode(viewer)
     addDebug(viewer)
-    this.iconPath = "./res/pic/marker.png"
-    this.polylinePath = "./res/pic/circle.png"
-    this.setCamera(viewer)
     this.drawState = DrawState.NONE;
-    this.getPositions = this.getPositions.bind(this)
-    // let cesiumDrawingTool = new CesiumDrawingTool(
-    //   viewer, // cesium viewer object
-    //   'cesiumContainer', // viewer DOM id
-    //   './webgis-src/demo/draw/pin.png' // location pin image uri
-    // );
-    // cesiumDrawingTool.init();
 
-    this.positions = [];
-    this.cesiumContainer = document.querySelector("#cesiumContainer")
-    this.initEvent();
-    this.initPoint();
-    this.initPolyline();
+    this._polylineTool = new PolylineTool(this._viewer)
+    this._markerTool = new MarkerTool(this._viewer);
+    this._polygonTool = new PolygonTool(this._viewer);
+    this._cesiumContainer = document.querySelector("#cesiumContainer");
+
+    this._setCamera()
+    this._initEvent();
+    this._initPointUI();
+    this._initPolylineUI();
+    this._initPolygonUI();
   }
 
-  initPoint() {
+  _initPointUI() {
     const dom = document.querySelector(".leaflet-pm-icon-marker")
     const meun = document.querySelector(".marker-menu");
 
     dom.addEventListener("click", () => {
       meun.style.display = "block"
       this.drawState = DrawState.MARKER;
-
-      if (!this.shape) {
-        this.shape = this.getMarker()
-      }
-      this.shape.show = true;
-      this.shape.position = this.lastPosition;
+      this._markerTool.enable = true;
     })
 
 
     meun.addEventListener("click", () => {
       meun.style.display = "none"
       this.drawState = DrawState.NONE;
-      this.shape.show = false;
-      this.shape = null;
+      this._markerTool.enable = false;
     })
   }
 
-  initPolyline() {
+  _initPolylineUI() {
     const dom = document.querySelector(".leaflet-pm-icon-polyline")
     const meun = document.querySelector(".polyline-menu");
+    const finish = document.querySelector(".polyline-menu .action-finish");
+    const cancel = document.querySelector(".polyline-menu .action-cancel");
 
-    //     cursor: pointer;default;crosshair
+    // cursor: pointer;default;crosshair
     dom.addEventListener("click", () => {
       meun.style.display = "block"
-      this.cesiumContainer.style.cursor = "pointer"
+      this._cesiumContainer.style.cursor = "pointer"
       this.drawState = DrawState.POLYLINE
-      if (!this.polylineMarker) {
-        this.polylineMarker = this.getPolylineMarker()
-      }
-      this.polylineMarker.position = this.lastPosition;
-
-      this.polylineMarker = this.viewer.entities.add({
-        polyline: {
-          positions: new Cesium.CallbackProperty(this.getPositions, false),
-          clampToGround: true,
-          width: 5,
-          material: Cesium.Color.RED
-        }
-      });
-      // if (!this.shape) {
-      //   this.shape = this.getMarker()
-      // }
-      // this.shape.show = true;
-      // this.shape.position = this.lastPosition;
+      this._polylineTool.enable = true;
     })
 
 
-    meun.addEventListener("click", () => {
+    finish.addEventListener("click", () => {
       meun.style.display = "none"
-      this.cesiumContainer.style.cursor = "default"
+      this._cesiumContainer.style.cursor = "default"
       this.drawState = DrawState.NONE;
-      // this.shape.show = false;
-      this.shape = null;
+      this._polylineTool.enable = false;
+      this._polylineTool.finish();
+    })
+
+    cancel.addEventListener("click", () => {
+      meun.style.display = "none"
+      this._cesiumContainer.style.cursor = "default"
+      this.drawState = DrawState.NONE;
+      this._polylineTool.enable = false;
+      this._polylineTool.cancle();
     })
   }
 
-  setCamera(viewer) {
+  _initPolygonUI() {
+    const dom = document.querySelector(".leaflet-pm-icon-polygon")
+    const meun = document.querySelector(".polygon-menu");
+    // const finish = document.querySelector(".polygon-menu .action-finish");
+    const cancel = document.querySelector(".polygon-menu .action-cancel");
 
-    cesiumContainer.style.opacity = 0;
+    // cursor: pointer;default;crosshair
+    dom.addEventListener("click", () => {
+      meun.style.display = "block"
+      this._cesiumContainer.style.cursor = "pointer"
+      this.drawState = DrawState.POLYLINE
+      this._polygonTool.enable = true;
+    })
+
+
+    // finish.addEventListener("click", () => {
+    //   meun.style.display = "none"
+    //   this._cesiumContainer.style.cursor = "default"
+    //   this.drawState = DrawState.NONE;
+    //   this._polygonTool.enable = false;
+    //   this._polygonTool.finish();
+    // })
+
+    cancel.addEventListener("click", () => {
+      meun.style.display = "none"
+      this._cesiumContainer.style.cursor = "default"
+      this.drawState = DrawState.NONE;
+      this._polygonTool.enable = false;
+      this._polygonTool.cancle();
+    })
+  }
+
+  _setCamera() {
+    this._cesiumContainer.style.opacity = 0;
     let info = {};
     info.lon = 116.4715397068917;
     info.lat = 39.9945712104195;
-    viewer.camera.flyTo({
+    this._viewer.camera.flyTo({
       destination: Cesium.Cartesian3.fromDegrees(info.lon, info.lat, 10000.0),
-      duration: 2,
+      duration: 0,
       complete: () => {
-        cesiumContainer.style.opacity = 1;
+        this._cesiumContainer.style.opacity = 1;
       }
     })
   }
 
-  getMarker() {
-    return this.viewer.entities.add({
-      position: this.lastPosition,
-      billboard: {
-        image: this.iconPath,
-        scale: 0.5,
-        verticalOrigin: Cesium.VerticalOrigin.BOTTOM
-      }
-    });
-  }
-
-  getPolylineMarker() {
-    return this.viewer.entities.add({
-      position: this.lastPosition,
-      billboard: {
-        image: this.polylinePath,
-        scale: 1,
-        width: 14,
-        height: 14
-        // verticalOrigin: Cesium.VerticalOrigin.CENTER
-      }
-    });
-  }
-
-  getPositions() {
-    return this.positions;
-  }
-  initEvent() {
-    var handler = new Cesium.ScreenSpaceEventHandler(this.viewer.scene.canvas);
+  _initEvent() {
+    var handler = new Cesium.ScreenSpaceEventHandler(this._viewer.scene.canvas);
     handler.setInputAction(event => {
-      this.lastPosition = this.viewer.scene.camera.pickEllipsoid(event.endPosition);
+      if (this.drawState === DrawState.NONE) return;
 
-      if (this.drawState === DrawState.MARKER) {
-        if (this.shape) {
-          this.shape.position = this.lastPosition;
-        }
-      } else if (this.drawState === DrawState.POLYLINE) {
-        if (this.polylineMarker) {
-          this.polylineMarker.position = this.lastPosition;
-        }
-
-        if (this.polylineMarker && this.positions.length>=1) {
-          this.positions[this.positions.length - 1] = this.lastPosition;
-          // this.shape.position = this.lastPosition;
-        }
-      }
-
+      const position = this._viewer.scene.camera.pickEllipsoid(event.endPosition);
+      this._polylineTool.mouseMove(position);
+      this._markerTool.mouseMove(position);
+      this._polygonTool.mouseMove(position);
     }, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
 
     handler.setInputAction(event => {
+      if (this.drawState === DrawState.NONE) return;
 
-      if (this.drawState === DrawState.MARKER) {
-        if (this.shape) {
-          this.shape = this.getMarker()
-          this.shape.position = this.lastPosition
-        }
-      } else if (this.drawState === DrawState.POLYLINE) {
-        this.positions.push(this.lastPosition);
-        this.polylineMarker = this.getPolylineMarker();
-        // if (!this.polylineMarker) {
-          // var dynamicPositions = new Cesium.CallbackProperty((function () {
-          //   return this.positions;
-          // }).bind(this), false);
-
-          // this.polylineMarker = this.viewer.entities.add({
-          //   polyline: {
-          //     positions: new Cesium.CallbackProperty(this.getPositions,false),
-          //     clampToGround: true,
-          //     width: 5,
-          //     material: Cesium.Color.RED
-          //   }
-          // });
-        // }
-
-      }
-
+      const position = this._viewer.scene.camera.pickEllipsoid(event.position);
+      this._polylineTool.mouseClick(position);
+      this._markerTool.mouseClick(position);
+      this._polygonTool.mouseClick(position);
     }, Cesium.ScreenSpaceEventType.LEFT_CLICK)
   }
 }
---
+++ webgis-src/demo/draw/MarkerTool.js
@@ -0,0 +1,49 @@
+export class MarkerTool {
+  constructor(viewer) {
+    this._viewer = viewer
+    this._activeMarker = null;
+    this._markers = [];
+    this._iconPath = "./res/pic/marker.png"
+  }
+
+  get enable() {
+    return this._enable;
+  }
+
+  set enable(v) {
+    this._enable = v;
+
+    if (v === false) {
+      this._viewer.entities.remove(this._activeMarker);
+      this._activeMarker = null;
+    }
+  }
+
+  _addMarker(position) {
+    return this._viewer.entities.add({
+      position: position,
+      billboard: {
+        image: this._iconPath,
+        scale: 0.5,
+        verticalOrigin: Cesium.VerticalOrigin.BOTTOM
+      }
+    });
+  }
+
+  mouseMove(position) {
+    if (!this._enable) return;
+
+    if (!this._activeMarker) {
+      this._activeMarker = this._addMarker(position);
+    } else {
+      this._activeMarker.position = position;
+    }
+  }
+
+  mouseClick(position) {
+    if (!this._enable) return;
+
+    this._markers.push(this._activeMarker)
+    this._activeMarker = this._addMarker(position);
+  }
+}
