775f8eafe73a649f87a3fbd65abb2d89ec2317f1
lianming <1097364388@qq.com>
2022-11-15 21:57:15 +0800

    add rangeMesh

--- webgis-src/demo/mesh/index.js
+++ webgis-src/demo/mesh/index.js
@@ -8,7 +8,7 @@ function fly(viewer, dist) {
     destination: dist,
     orientation: {
       heading: 0,
-      pitch: -Math.PI / 3,
+      pitch: -Math.PI / 2,
       roll: 6.283185307179586,
     },
     duration: 0
@@ -16,27 +16,25 @@ function fly(viewer, dist) {
   viewer.scene.camera.flyTo(flyToOpts);
 }
 
+// 添加辐射范围
 export function addRangeMesh(viewer) {
-  var position = Cesium.Cartesian3.fromDegrees(106, 26, 0);
+  var position = Cesium.Cartesian3.fromDegrees(116.4715397068917, 39.9945712104195, 0);
 
   const range = new RangeMesh({
     viewer,
+    // 圆心
     center: position,
     // 半径范围
-    radiusArr: [
-      0,
-      50,
-      // 90,
-      // 150
-    ],
-    // 范围颜色
+    radiusArr: [0, 50, 90, 150],
+    // 颜色
     colorArr: [
-      "rgba(255,0,0,0.5)",
-      "rgba(0,255,0,0.5)",
-      "rgba(0,0,255,0.5)"
+      "rgba(255,0,0,0.7)",
+      "rgba(0,255,0,0.7)",
+      "rgba(0,0,255,0.7)"
     ]
-  })
+  });
 
-  onMouseWheel(viewer, range.updateLabel)
-  fly(viewer, Cesium.Cartesian3.fromDegrees(106, 26, 5000));
+  // onMouseWheel(viewer, range.updateLabel)
+  viewer.scene.postRender.addEventListener(range.updateLabel, range);
+  fly(viewer, Cesium.Cartesian3.fromDegrees(116.4715397068917, 39.9945712104195, 5000));
 }
--- webgis-src/mesh/RangeMesh.js
+++ webgis-src/mesh/RangeMesh.js
@@ -24,8 +24,6 @@ export class RangeMesh {
     this.labels = [];
 
     for (let i = 0; i < radiusArr.length - 1; i++) {
-      console.error(123);
-
       const innerRadius = radiusArr[i]
       const outerRadius = radiusArr[i + 1]
       const color = colorArr[i];
@@ -45,38 +43,42 @@ export class RangeMesh {
 
   updateLabel() {
     const scene = this._viewer.scene;
-    const r = scene.mapProjection.ellipsoid;
-    const i = scene.camera.positionWC;
-    const n = new Cesium.EllipsoidalOccluder(r, i);
     const labelCollection = this._viewer.dataSourceDisplay._defaultDataSource._visualizers[2]._cluster
-
     var l = [];
+    const cartesian2 = new Cesium.Cartesian2();
 
     for (let i = 0; i < this.labels.length; i++) {
       const entity = this.labels[i];
       const label = labelCollection.getLabel(entity)
-
-      var pos = label.computeScreenSpacePosition(scene);
+      Cesium.SceneTransforms.wgs84ToWindowCoordinates(scene, entity.position._value, cartesian2);
+      const pos = label.computeScreenSpacePosition(scene);
       const bound = Cesium.Label.getScreenSpaceBoundingBox(
         label,
         pos,
         new Cesium.BoundingRectangle()
       );
-      const cartesian2 = new Cesium.Cartesian2();
-      Cesium.SceneTransforms.wgs84ToWindowCoordinates(scene, entity.position._value, cartesian2);
 
-      const dom = document.querySelector(`.test${i+1}`);
+      if (!entity.ueserData && bound.height !== -Infinity) {
+        entity.ueserData = {};
+        entity.ueserData.bound = Cesium.BoundingRectangle.clone(bound);
+      }
+
+      if (!entity.ueserData) continue;
+
+      // const dom = document.querySelector(`.test${i+1}`);
       // dom.style.width = bound.width + "px"
       // dom.style.height = bound.height + "px"
-      dom.style.top = cartesian2.y - bound.height / 2  + "px"
-      dom.style.left = cartesian2.x - bound.width / 2 + "px"
+      // dom.style.top = cartesian2.y - bound.height / 2  + "px"
+      // dom.style.left = cartesian2.x - bound.width / 2 + "px"
 
+      entity.ueserData.bound.x = cartesian2.x;
+      entity.ueserData.bound.y = cartesian2.y;
 
-      if (isIntersect(l, bound)) {
+      if (isIntersect(l, entity.ueserData.bound)) {
         entity.show = false;
       } else {
         entity.show = true;
-        l.push(Cesium.BoundingRectangle.clone(bound));
+        l.push(Cesium.BoundingRectangle.clone(entity.ueserData.bound));
       }
     }
   }
@@ -92,7 +94,7 @@ export class RangeMesh {
     const position = Cesium.Cartesian3.fromDegrees(
       labelDegrees[0],
       labelDegrees[1],
-      10);
+      0);
 
     const label = this._viewer.entities.add({
       position,
@@ -101,16 +103,19 @@ export class RangeMesh {
         verticalOrigin: Cesium.VerticalOrigin.CENTER,
         text,
         scale: 0.5,
-        font: '34px Helvetica',
+        font: '700 50px 微软雅黑',
         pixelOffset: new Cesium.Cartesian2(0, 0),
         eyeOffset: new Cesium.Cartesian3(0, 0, -100),
         fillColor: Cesium.Color.WHITE,        //字体颜色
         style: Cesium.LabelStyle.FILL,        //label样式
         outlineWidth: 2,
-        showBackground: true,
+        outlineColor: Cesium.Color.BLACK,
+        showBackground: false,
+        // backgroundColor: new Cesium.Color(0,0,0,0.4),
         distanceDisplayCondition: new Cesium.DistanceDisplayCondition(0, 50000),
       }
     });
+    // label
 
     this.labels.push(label);
   }
